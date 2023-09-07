f8ea600eee1b5038204c3fac4e4a0da261c35a92
zgm <lovemylife_zgm@outlook.com>
2023-09-07 17:19:18 +0800

    显示线程一

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 // mod  vector_fun;
 // use vector_fun::sort_struct as Sort;
 
-mod rand_fun;
-use rand_fun::rng_alp_range  as Rand;
+// mod rand_fun;
+// use rand_fun::rng_alp_range  as Rand;
+
+mod cross_fun;
+use cross_fun::find_max as Cross;
 
 fn main() {
     
     // Sort();
-    Rand();
+
+    // Rand();
+
+    let arr = &[1, 25, -4, 10];
+    let max = Cross(arr);
+    println!("max: {:#?}", max);
 
 }
